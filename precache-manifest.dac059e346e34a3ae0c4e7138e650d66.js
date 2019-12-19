cbb14958ecdf691da78aba2ef97aa13eaec8ab86
Daniele Sforza <danielesforza@gmail.com>
2019-12-19 15:19:08 +0100

    Updates

---
+++ precache-manifest.dac059e346e34a3ae0c4e7138e650d66.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad2f652a0655a01ab6923a3e724fed1c",
+    "url": "/reactnd-project-myreads/index.html"
+  },
+  {
+    "revision": "b3bf51f22731dff84a31",
+    "url": "/reactnd-project-myreads/static/css/main.7a09d23e.chunk.css"
+  },
+  {
+    "revision": "566958b82ca02e3a4744",
+    "url": "/reactnd-project-myreads/static/js/2.80775036.chunk.js"
+  },
+  {
+    "revision": "adf988142ef3edd1ada16f48aab2de8c",
+    "url": "/reactnd-project-myreads/static/js/2.80775036.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3bf51f22731dff84a31",
+    "url": "/reactnd-project-myreads/static/js/main.cf330eb8.chunk.js"
+  },
+  {
+    "revision": "22174e84a1c0910f7eef",
+    "url": "/reactnd-project-myreads/static/js/runtime-main.7d7d82df.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/reactnd-project-myreads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/reactnd-project-myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/reactnd-project-myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
